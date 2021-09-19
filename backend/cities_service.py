3de3d92253f76db9f7439c534cedd92b87102063
Anna Moelling <anna.moelling@googlemail.com>
2021-09-19 18:47:55 +0200

    use a loop for loading weather of all predefined cities

--- backend/cities_service.py
+++ backend/cities_service.py
@@ -2,11 +2,14 @@
 import requests
 
 class CityService:
+    cities = [ "Berlin", "Leipzig", "Mountain View" ] # will be 10 cities at the end
 
     def get_all(self):
+      data = []
+      for city in CityService.cities:
         response = requests.get(
-            "https://api.openweathermap.org/data/2.5/weather?q=Berlin&appid=33723607922dc4022ad15bf945b658cb")
+            f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid=33723607922dc4022ad15bf945b658cb")
 
-        city = response.json()
+        data.append(response.json())
 
-        return [city]
+      return data
