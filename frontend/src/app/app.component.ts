27d74f7676c0dedfd6c3f2ac93f5b0b5e98a8757
Anna Moelling <anna.moelling@googlemail.com>
2021-09-25 19:25:16 +0200

    add city interface and city mock

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
-
+import { City } from './city';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'my-app';
+  title = 'Weather App';
+  cities: City[] = [
+    {
+      id: 1,
+      name: 'Berlin',
+      temperature: 10.4,
+    },
+    {
+      id: 1,
+      name: 'Barcelona',
+      temperature: 15.4,
+    },
+  ];
 }
